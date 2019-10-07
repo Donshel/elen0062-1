fb28f00a07248dd16c997e28c3a2a111f1918ebe
François Rozet <francois.rozet@outlook.com>
2019-10-07 14:41:59 +0200

    3. Naive Bayes classifier

--- 1/python/naive_bayes.py
+++ 1/python/naive_bayes.py
@@ -10,13 +10,20 @@ import numpy as np
 
 from sklearn.base import BaseEstimator
 from sklearn.base import ClassifierMixin
+from sklearn.utils import check_X_y, check_array
+from sklearn.utils.validation import check_is_fitted
 
 from data import make_data1, make_data2
 from plot import plot_boundary
 
 
-class GaussianNaiveBayes(BaseEstimator, ClassifierMixin):
+# 3 Naive Bayes classifier
 
+# 3.2 Classifier
+class NaiveBayesClassifier(BaseEstimator, ClassifierMixin):
+
+    def _more_tags(self):
+        return {'requires_fit': True}
 
     def fit(self, X, y):
         """Fit a Gaussian navie Bayes model using the training set (X, y).
@@ -34,19 +41,30 @@ class GaussianNaiveBayes(BaseEstimator, ClassifierMixin):
         self : object
             Returns self.
         """
-        # Input validation
-        X = np.asarray(X, dtype=np.float)
-        if X.ndim != 2:
-            raise ValueError("X must be 2 dimensional")
 
-        y = np.asarray(y)
-        if y.shape[0] != X.shape[0]:
-            raise ValueError("The number of samples differs between X and y")
+        # Check that X and y have correct shape
+        X, y = check_X_y(X, y)
 
-        # ====================
-        # TODO your code here.
-        # ====================
+        # Classes
+        self.classes_, y = np.unique(y, return_inverse = True)
+        self.n_classes = len(self.classes_)
 
+        # Variables
+        self.n_variables = X.shape[1]
+
+        # Prior
+        self.prior = np.bincount(y)
+
+        # Means and variances
+        self.mu = np.zeros((self.n_classes, self.n_variables))
+        self.var = np.copy(self.mu)
+
+        for i in range(self.n_classes):
+            for j in range(self.n_variables):
+                self.mu[i][j] = np.mean(X[y == i, j])
+                self.var[i][j] = np.var(X[y == i, j])
+
+        # Return the classifier
         return self
 
     def predict(self, X):
@@ -63,13 +81,9 @@ class GaussianNaiveBayes(BaseEstimator, ClassifierMixin):
             The predicted classes, or the predict values.
         """
 
-        # ====================
-        # TODO your code here.
-        # ====================
+        return self.classes_[np.argmax(self.predict_proba(X, False), axis = 1)]
 
-        pass
-
-    def predict_proba(self, X):
+    def predict_proba(self, X, normalize = True):
         """Return probability estimates for the test data X.
 
         Parameters
@@ -84,12 +98,54 @@ class GaussianNaiveBayes(BaseEstimator, ClassifierMixin):
             by lexicographic order.
         """
 
-        # ====================
-        # TODO your code here.
-        # ====================
+        # Check is fit had been called
+        check_is_fitted(self, ["classes_"])
 
-        pass
+        # Check that X has correct shape
+        check_array(X)
+        if X.shape[1] != self.n_variables:
+            raise ValueError("X must have %d variables" % self.p)
+
+        # Probabilities
+        y = np.zeros((X.shape[0], self.n_classes))
+
+        for n in range(X.shape[0]):
+            for i in range(self.n_classes):
+                temp = self.mu[i] - X[n]
+                temp = temp ** 2
+                temp = temp / self.var[i]
+                temp = np.exp(-np.sum(temp) / 2)
+                temp = temp / np.sqrt(np.prod(self.var[i]))
+                temp = self.prior[i] * temp
+                
+                y[n][i] = temp
+
+            if normalize:
+                y[n] /= np.sum(y[n])
+
+        return y
 
 if __name__ == "__main__":
-    from data import make_data
-    from plot import plot_boundary
+    # Parameters
+    make_data = [make_data1, make_data2]
+    m, n, g = 2000, 150, 5
+
+    # 3.3 Accuracies
+    print("make_data", "mean", "std")
+
+    for i in range(len(make_data)):
+        # Generations
+        X, y = [None] * g, [None] * g
+        for j in range(g):
+            X[j], y[j] = make_data[i](m, random_state = j)
+
+        # Classifiers
+        A = np.zeros(g)
+
+        for j in range(g):
+            nbc = NaiveBayesClassifier()
+            nbc.fit(X[j][0:n], y[j][0:n])
+
+            A[j] = nbc.score(X[j][n:m], y[j][n:m])
+        
+        print(str(i + 1), "%f" % np.mean(A), "%f" % np.std(A))
